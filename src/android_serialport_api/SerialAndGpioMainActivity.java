4af5cbbb3a644c96dcb456b3bf13d9568fc1bb9e
crazyStrome <1415441419@qq.com>
2017-11-22 10:38:54 +0800

    修改了AndroidMainfest.xml

--- src/android_serialport_api/SerialAndGpioMainActivity.java
+++ src/android_serialport_api/SerialAndGpioMainActivity.java
@@ -4,16 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android_serialport_api.sample.R;
+import android_serialport_api.sample.SerialPortActivity;
 
 /**
  * Created by HSP on 2017/11/19.
  */
 
-public class SerialAndGpioMainActivity extends Activity {
+public class SerialAndGpioMainActivity extends SerialPortActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serial_and_gpio_activity_main);
+    }
+
+    @Override
+    protected void onTty2DataReceived(byte[] buffer, int size) {
+    }
+
+    @Override
+    protected void onTty3DataReceived(byte[] buffer, int size) {
     }
 }
